22b7aa7cb1d4c47f134fe266615fc5644ec942f5
frankong <frankong@berkeley.edu>
2018-08-06 18:45:03 -0700

    Newer version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
     
 setup(name='mridata',
-      version='0.2.0',
+      version='0.2.1',
       description='Python command line interface for downloading and uploading to mridata.org.',
       long_description=long_description,
       long_description_content_type="text/markdown",
