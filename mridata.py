525dae757039ffbc3ded7599ab6cfa4c751acd54
frankong <frankong@berkeley.edu>
2018-08-06 18:14:42 -0700

    Added batch download command.

--- mridata.py
+++ mridata.py
@@ -42,10 +42,17 @@ def download(uuid):
     total_size = int(r.headers.get('content-length', 0))
     chunk_size = 1024
     total_chunks = (total_size + chunk_size - 1) // chunk_size
+    print('Downloading {}...'.format(uuid))
     with open('{}.h5'.format(uuid), 'wb') as f:
         for chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=total_chunks, unit='KB'):
             if chunk:
                 f.write(chunk)
+
+
+def batch_download(filename):
+    uuids = open(filename).read().splitlines()
+    for uuid in uuids:
+        download(uuid)
 
 
 def get_temporary_credentials(session):
